2f0fcb680e7ef180f5f225f4d33bea22a4f020c5
Bart Franse <bartfranse@ip247-048.stu-wireless.HZeeland.nl>
2018-09-06 14:58:40 +0200

    initial bart demo

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+    public Text waveCountdownText;
+
+
+	void Update()
+	{
+
+        if(countdown <= 0f){
+            StartCoroutine(Spawnwave());
+            countdown = timeBetweenWaves;
+
+        }
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(countdown).ToString();
+		
+	}
+
+    IEnumerator Spawnwave(){
+        
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+
+    }
+
+    void spawnEnemy(){
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
